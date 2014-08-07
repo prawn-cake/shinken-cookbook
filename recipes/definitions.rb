100a2531dff6f7b3b57a11d5ee9d8952cba62734
Eric Herot <eric.github@herot.com>
2014-08-07 19:39:37 -0400

    Only try to set up users and admins groups if we have users in the first place

--- recipes/definitions.rb
+++ recipes/definitions.rb
@@ -21,32 +21,34 @@ if credentials['shinken']
       notifies :restart, 'service[shinken]'
     end
   end
-end
 
-template '/etc/shinken/contactgroups/admins.cfg' do
-  source 'generic-contactgroups.cfg.erb'
-  owner  node['shinken']['user']
-  group  node['shinken']['group']
-  mode   0644
-  variables(
-    cg_name: 'admins',
-    cg_alias: 'admins',
-    members: shinken_users.select { |u| u['shinken']['is_admin'] == '1' }.map { |u| u['id'] }
-  )
-  notifies :restart, 'service[shinken]'
-end
+  template '/etc/shinken/contactgroups/admins.cfg' do
+    source 'generic-contactgroups.cfg.erb'
+    owner  node['shinken']['user']
+    group  node['shinken']['group']
+    mode   0644
+    variables(
+      cg_name: 'admins',
+      cg_alias: 'admins',
+      members: shinken_users.select { |u| u['shinken']['is_admin'] == '1' }.map { |u| u['id'] }
+    )
+    notifies :restart, 'service[shinken]'
+  end
 
-template '/etc/shinken/contactgroups/users.cfg' do
-  source 'generic-contactgroups.cfg.erb'
-  owner  node['shinken']['user']
-  group  node['shinken']['group']
-  mode   0644
-  variables(
-    cg_name: 'users',
-    cg_alias: 'users',
-    members: shinken_users.map { |u| u['id'] }
-  )
-  notifies :restart, 'service[shinken]'
+  template '/etc/shinken/contactgroups/users.cfg' do
+    source 'generic-contactgroups.cfg.erb'
+    owner  node['shinken']['user']
+    group  node['shinken']['group']
+    mode   0644
+    variables(
+      cg_name: 'users',
+      cg_alias: 'users',
+      members: shinken_users.map { |u| u['id'] }
+    )
+    notifies :restart, 'service[shinken]'
+  end
+else
+  Chef::Log.warn 'No secret credentials found.'
 end
 
 node['shinken']['services'].each do |svc_name, svc_conf|
