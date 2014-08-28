1cfeb6be67627f3a9f0f15325d99799bdaeaa8d8
Eric Herot <eric.github@herot.com>
2014-08-28 15:00:56 -0400

    Version bump v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.2
+
+* Add check_http command
+
 # 1.0.1
 
 Initial release of shinken
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures shinken'
 long_description 'Installs/Configures shinken'
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'apt'
 depends 'python'
